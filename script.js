fe0b1603dfbc4663ed187b2b865d07962ef7d67f
Ullas Khamitkar <63913996+ullaskhamitkar@users.noreply.github.com>
2025-06-09 19:05:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+console.log("Website loaded");
